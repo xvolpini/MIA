1d2e8f15e431449782b7c0299445168b7801c27f
Ximena Volpini <ximenavolpini@gmail.com>
2026-01-17 00:06:17 -0300

    Uso de credenciales desde st.secrets

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ scope = [
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
+import streamlit as st
+from google.oauth2.service_account import Credentials
+
 credenciales = Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=scope
